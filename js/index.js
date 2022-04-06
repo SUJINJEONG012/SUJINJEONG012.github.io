92313bf05eed2a48dfe01c05674f00673c281942
JEONGSUJIN012 <peekaboo32@naver.com>
2022-04-06 23:53:39 +0900

    포트폴리오 스크립트 기능 스타일수정

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 //토글 열고 닫기
 const html = document.querySelector('html');
 const header = html.querySelector('.main_header');
@@ -9,6 +11,45 @@ toggleBtn.addEventListener('click', () =>{
     header.classList.toggle('active');
     headerOver.classList.toggle('active');
 });
+
+
+
+
+
+//타이밍효과
+
+const content = '안녕하세요. \n 프론트개발을 하고 싶은 퍼블리셔 정수진입니다.';
+const text = document.querySelector('.text');
+let i = 0;
+
+function sleep(delay){
+    const start = new Date().getTime();
+    while (new Date().getTime()< start+delay);
+}
+// function typing(){
+//     text.textContent += content[i++];
+//     if(i > content.length){
+//         text.textContent = ''
+//         i = 0;
+//         sleep(3000);
+//     }
+// }
+
+
+function typing(){
+    let txt = content[i++];
+    text.innerHTML += txt=== "\n" ? "<br/>": txt;
+    if (i > content.length) {
+        text.textContent = "";
+        i = 0;
+        sleep(3000);
+    }
+}
+
+
+setInterval(typing, 200);
+
+
 
 
 
