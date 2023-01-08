b63d7763f618a14399f585f2d99c8b480e85657a
SUJINJEONG012 <peekaboo32@naver.com>
2023-01-09 03:40:59 +0900

    모달창 구현

--- js/index.js
+++ js/index.js
@@ -26,6 +26,29 @@ setInterval(typing, 200);
 
 
 
+//모달창
+
+//필요한 엘리먼트들을 선택한다.
+const openBtn = document.getElementById('open');
+const modal = document.querySelector('.modal');
+const overlay = modal.querySelector('.md_overlay');
+const closeBtn = modal.querySelector('button');
+
+
+//동작 함수
+
+const openModal = () => {
+    modal.classList.remove('hidden');
+}
+const closeModal = () => {
+    modal.classList.add('hidden');
+}
+
+//클릭이벤트
+
+openBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
 
 
 
