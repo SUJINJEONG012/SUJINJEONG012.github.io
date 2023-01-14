5e6e350e513750f250988bfe65d8584d45923789
SUJINJEONG012 <peekaboo32@naver.com>
2023-01-15 03:14:16 +0900

    모달창구현완료

--- js/index.js
+++ js/index.js
@@ -2,13 +2,15 @@
 let btn = document.querySelectorAll("button.modal-custom-button");
 let modals = document.querySelectorAll(".modal-custom");
 let spans = document.getElementsByClassName("close-modal");
+let overlays = document.querySelectorAll(".md_overlay");
 
 for (let i = 0; i < btn.length; i++) {
     btn[i].onclick = function (e) {
         e.preventDefault();
         modal = document.querySelector(e.target.getAttribute("href"));
-        modal.style.display = "block";
+        modal.style.display = "flex";
     }
+
 
     spans[i].onclick = function () {
         for (let index in modals) {
@@ -17,13 +19,12 @@ for (let i = 0; i < btn.length; i++) {
         }
     }
 
-}
-
-
-window.onclick = function (event) {
-    if (event.target.classList.contains('modal-custom')) {
-        for (let index in modals) {
-            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+    overlays[i].onclick = function () {
+        for (let overlay in modals) {
+            if (typeof modals[overlay].style !== 'undefined')
+                modals[overlay].style.display = "none";
         }
     }
+
+
 }
